66de10140c630433373fbfdd1c361aa087943232
Sean Lim <sean.lim@versent.com.sg>
2024-06-01 13:09:58 +0800

    adds dependency list

---
+++ demo-0.py
@@ -0,0 +1,27 @@
+# Run Python - python / python3
+
+# Data types 
+#   numbers
+123
+123.12 
+#   string
+"hello world"
+"hello"
+"h"
+# list
+[1, 2, 3, 4]
+# dict
+{ "name": "sara" }
+
+# If Else
+a = 10
+if a > 10:
+    print("Thie is a big number")
+else:
+    print("This is a small number")
+
+# Loops (while)
+a = 0
+while a < 10:
+    print(f"a: {a}")
+    a = a + 1
---
+++ demo-1.py
@@ -0,0 +1,21 @@
+# If Else
+a = 10
+if a > 10:
+    print("Thie is a big number")
+    print("...")
+else:
+    print("This is a small number")
+
+# Loops (while)
+a = 0
+while a < 10:
+    print(f"a: {a}")
+    a = a + 1
+
+# Function
+def hello(name):
+    print(f"Hello World, {name}")
+
+hello("Jeff")       # <- valid string
+hello('Jeff')       # <- valid string
+# hello(Jeff)         # <- invalid variable
