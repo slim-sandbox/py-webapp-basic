487c149bf3b886dc4c8eee9905d9b49efc9821ec
Sean Lim <9212876+seanlim1@users.noreply.github.com>
2024-08-24 00:45:57 +0800

    uses environment variables to set name to greet

--- web-flask.py
+++ web-flask.py
@@ -1,11 +1,14 @@
 # Simple web application using Flask framework
 from flask import Flask
+import os
+
+HELLO_NAME = os.getenv('APP_HELLO_NAME', "World")
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"<p>Hello, {HELLO_NAME}!</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
